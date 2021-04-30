3a43a0e916727d58fdba73a39a2bd03cc385bfd9
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2021-04-30 12:13:20 -0500

    Update server to connect to remote mongodb

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const routes = require('./routes/api')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000
+
+mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log(`Database connected successfully!`))
+    .catch(err => console.log(err))
+
+mongoose.Promise = global.Promise
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
@@ -10,8 +20,13 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use((req, res, next) => {
-    res.send('Express!')
+app.use(bodyParser.json())
+
+app.use('/api', routes)
+
+app.use((err, req, res, next)=> {
+    console.log(err)
+    next()
 })
 
 app.listen(port, () => {
